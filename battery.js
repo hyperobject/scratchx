d025dbd4bcbcd067ca4681a6dfe277e186174b9f
Connor <technoboy10@users.noreply.github.com>
2015-05-11 15:18:49 -0600

    Create battery.js

---
+++ battery.js
@@ -0,0 +1,34 @@
+(function(ext) {
+    var battery = navigator.battery || navigator.webkitBattery || navigator.mozBattery || navigator.msBattery;
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        if (battery) {
+        return {status: 2, msg: 'Ready'};
+        } else {
+        return {status: 1, msg: 'Battery API not supported - use Firefox!'};
+    };
+    
+    ext.val = function(){
+		return (battery.level * 100);
+	};
+	
+	  ext.charging = function(){
+	  return battery.charging;
+	  }
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['r', 'battery percentage', 'val'],
+            ['b', 'battery charging?', 'charging']
+        ],
+        url: 'http://technoboy10.tk/'
+    };
+
+
+    // Register the extension
+    ScratchExtensions.register('Battery Status', descriptor, ext);
+})({});
