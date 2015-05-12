96d55e3a7212c94ec063e4c8b6b1ee57ce3b495a
Connor <technoboy10@users.noreply.github.com>
2015-05-11 19:51:48 -0600

    Create gamepad.js

---
+++ gamepad.js
@@ -0,0 +1,34 @@
+(function(ext) {
+    var gamepad = navigator.getGamepads()[0];
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        if (!navigator.getGamepads || !navigator.gamepads) {
+        return {status: 2, msg: 'Ready'};
+        } else {
+        return {status: 1, msg: 'Gamepad API not supported - use Firefox!'};
+        }
+    };
+    
+    ext.pressed = function(num){return gamepad.buttons[num - 1].pressed};
+    ext.when_pressed = function(num){return gamepad.buttons[num - 1].pressed};
+	
+	  ext.charging = function(){
+	  return battery.charging;
+	  }
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['h', 'when button %n pressed', 'when_pressed'],
+            ['b', 'button %n pressed?', 'pressed']
+        ],
+        url: 'http://technoboy10.tk/'
+    };
+
+
+    // Register the extension
+    ScratchExtensions.register('Gamepad', descriptor, ext);
+})({});
