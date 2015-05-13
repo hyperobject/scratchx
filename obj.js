ced3ca74e3a954a88fd54ba21015c1ed344c13d7
Technoboy10 <technoboy10@fake.email>
2015-05-13 07:56:17 -0600

    json

---
+++ obj.js
@@ -0,0 +1,33 @@
+(function(ext) {
+    
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+    
+    ext.grab = function(url){
+		$.getJSON('http://crossorigin.me/' + url, function(json){
+			callback(json);
+		});
+	};
+	
+	ext.parse = function(key, obj){
+		return $.parseJSON(obj)[key];
+	}
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['R', 'get JSON from URL %s', 'grab'],
+			['r', 'key %s from JSON %s', 'parse']
+        ],
+        url: 'http://technoboy10.tk/'
+    };
+
+
+    // Register the extension
+    ScratchExtensions.register('Cloud Data', descriptor, ext);
+})({});
